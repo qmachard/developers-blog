fa7edd6312c83c92da284b7a853b91730fe08d39
Quentin MACHARD <quentin.machard@gmail.com>
2020-01-27 23:10:19 +0100

    chore(TypeScript / Tests): Add first tests

--- src/utils/numbers.ts
+++ src/utils/numbers.ts
@@ -1,3 +1,14 @@
-export function formatNumber(num: number): string {
-  return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1\u00a0')
-}
+/**
+ * Format number to french
+ *
+ * @example 2000.34 -> '2 000,34'
+ *
+ * @param {number} num
+ *
+ * @return {string}
+ */
+export const formatNumber = (num: number): string =>
+  num
+    .toString()
+    .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1\u00a0')
+    .replace('.', ',');
